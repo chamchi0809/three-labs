48819fcab1095516c32dc01a631dad41b4fbd0dd
최지원 <66864266+chamchi0809@users.noreply.github.com>
2026-08-11 17:45:41 +0900

    change demo port number

--- sdfgi/demo/vite.config.ts
+++ sdfgi/demo/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // top-level await in src/main.ts needs an esnext target
 export default defineConfig({
-  build: { target: 'esnext' },
-  esbuild: { target: 'esnext' },
+  build: { target: "esnext" },
+  esbuild: { target: "esnext" },
+  server: {
+    port: 8124,
+  },
 });
